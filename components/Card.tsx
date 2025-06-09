09f233ac3dca121b97f52f429bfab92ccfa19ec5
tsunasan-html <lemoned.i.scream.15@gmail.com>
2025-06-09 13:02:20 +0900

    fixed

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,6 +3,9 @@ import Link from "next/link";
 import React, { FC } from "react";
 import { CardProps } from "../types/types";
 import { getCover, getDate, getMultiSelect, getText } from "../utils/property";
+
+const trimText = (text: string, max = 17) =>
+  text.length > max ? text.slice(0, max) + '...' : text;
  
 const Card: FC<CardProps> = ({ page }) => {
   return (
@@ -22,7 +25,7 @@ const Card: FC<CardProps> = ({ page }) => {
           {/* title & date*/}
           <div className="px-6 pt-4 ">
             <h2 className="text-base font-medium mb-3 ">
-              {getText(page.properties.name.title)}
+                {trimText(getText(page.properties.name.title))}
             </h2>
             <p className="text-gray-700 text-xs">
               {getDate(page.properties.published.date)}
